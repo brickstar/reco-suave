f149945a2f14eddba7c0ba39017aa5ec63f1a77e
brickstar <brickstar@gmail.com>
2018-11-04 14:34:35 -0700

    Add simplecov filters for accurate coverage report.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,6 +21,8 @@ SimpleCov.start 'rails' do
   add_filter "app/channels/application_cable/connection.rb"
   add_filter "app/jobs/application_job.rb"
   add_filter "app/mailers/application_mailer.rb"
+  add_filter "app/helpers/application_helper.rb"
+  add_filter "app/models/application_record.rb"
 end
 
 Shoulda::Matchers.configure do |config|
