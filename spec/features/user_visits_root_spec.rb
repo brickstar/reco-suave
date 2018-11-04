33ad8bcc7dc79c5ea345d3e101e9dda721787390
brickstar <brickstar@gmail.com>
2018-11-04 15:07:39 -0700

    Add route for recommendations index. Add feature spec sketch for recommendations workflow. Widen text field on welcome index" gs "

--- spec/features/user_visits_root_spec.rb
+++ spec/features/user_visits_root_spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 
 describe "user visits root path" do
-  context "should see a from to enter information to generate a recommendation" do
+  context "should see a form to enter information to generate a recommendation" do
     it "should have a placeholder of 'Bands, Movies, TV Shows, Authors, or Games...'" do
       visit '/'
 
-      expect(page).to have_field('Recommendations:')
+      expect(page).to have_field('q')
       expect(page).to have_xpath("//input[@value='Bands, Movies, TV Shows, Authors, or Games...']")
+    end
+  end
+  context 'fills in text field with a recommendation prompt' do
+    it 'should land on search#index and see related recommendations' do
+      visit '/'
+
+      fill_in "q", with: "Beatles"
+      click_on "Search"
+
+      expect(current_path).to eq(recommendations_path)
     end
   end
 end
